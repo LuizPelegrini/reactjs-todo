4aa4d1735851a1e935ab180acbeeabe8bd7bf9e8
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-10-11 23:01:05 +0800

    add: Scaffold AddNewTask form structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css';
+import { PlusCircle } from 'phosphor-react';
 
 import { Header } from './components/Header';
 
@@ -6,6 +7,16 @@ function App() {
   return (
     <div className={styles.container}>
       <Header />
+
+      <main className={styles.wrapper}>
+        <form className={styles.addTaskForm}>
+          <input type="text" className={styles.taskInput} placeholder="Add new task"/>
+          <button type="submit">
+            Create
+            <PlusCircle size={18}/>
+          </button>
+        </form>
+      </main>
     </div>
   )
 }
